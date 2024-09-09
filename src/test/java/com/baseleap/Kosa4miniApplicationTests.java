c0d5b2953725f1921841341d7636758dda476a7d
NoelClay <asdf1578@naver.com>
2024-09-09 11:46:35 +0900

    첫 커밋

---
+++ src/test/java/com/baseleap/Kosa4miniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baseleap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kosa4miniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
