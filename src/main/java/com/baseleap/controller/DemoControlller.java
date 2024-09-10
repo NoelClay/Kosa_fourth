74dd2bf2c52d1a472129f874ba53436fd6eede63
NoelClay <asdf1578@naver.com>
2024-09-10 18:33:55 +0900

    wqeqwe

--- src/main/java/com/baseleap/controller/DemoControlller.java
+++ src/main/java/com/baseleap/controller/DemoControlller.java
@@ -2,12 +2,16 @@ package com.baseleap.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class DemoControlller {
 	
 	@GetMapping("/demo")
 	public String demo() {
 		return "demo";
 	}
+	
 }
